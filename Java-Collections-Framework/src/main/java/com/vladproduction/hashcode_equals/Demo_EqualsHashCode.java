a2fedea9e41ec02fbcca47043f2678f9eef1ea46
vladproduction <vladbogdantsev@gmail.com>
2024-06-23 21:05:03 +0300

    hashCode and equals (example)

---
+++ Java-Collections-Framework/src/main/java/com/vladproduction/hashcode_equals/Demo_EqualsHashCode.java
@@ -0,0 +1,7 @@
+package com.vladproduction.hashcode_equals;
+
+public class Demo_EqualsHashCode {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java-Collections-Framework/src/main/java/com/vladproduction/hashcode_equals/TestMapSet.java
@@ -0,0 +1,27 @@
+package com.vladproduction.hashcode_equals;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TestMapSet {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+
+        map.put(1, "One");
+        map.put(1, "Other");
+
+        set.add(1);
+        set.add(1);
+
+        System.out.println(map);
+        System.out.println(set);
+
+        /*  {1=Other}
+            [1]
+        */
+    }
+}
